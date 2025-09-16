5473794e0708fc47b45797e78eaa7ba68068ad34
kqanGit <bmquan.GD@gmail.com>
2025-09-16 21:14:55 +0700

    exercise 1.4

--- part-1/src/App.jsx
+++ part-1/src/App.jsx
@@ -1,55 +1,74 @@
-
 const Header = (props) => {
   return (
     <>
       <h1>{props.course}</h1>
     </>
-  )
-}
+  );
+};
 
 const Part = (props) => {
-  console.log(props)
+  console.log(props);
   return (
     <>
-      <p>{props.part} {props.exercises}</p>
+      <p>
+        {props.part} {props.exercises}
+      </p>
     </>
-  )
-}
+  );
+};
 
-const Content = (props) => {
+const Content = ({ parts }) => {
   return (
     <>
-      <Part part={props.part1} exercises={props.exercises1} />
-      <Part part={props.part2} exercises={props.exercises2} />
-      <Part part={props.part3} exercises={props.exercises3} />
+      {parts.map((part) => {
+        return (
+          <p key={part.name}>
+            {part.name} {part.exercises}
+          </p>
+        );
+      })}
     </>
-  )
-}
+  );
+};
 
-const Total = (props) => {
+const Total = ({parts}) => {
+  let sum = 0;
+  parts.forEach(part => {sum += part.exercises})
   return (
     <>
-      <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+      <p>
+        Number of exercises: {sum}
+      </p>
     </>
   )
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const course = "Half Stack application development";
+  const parts = [
+    {
+      name: "Fundamentals of React",
+      exercises: 10,
+    },
+    {
+      name: "Using props to pass data",
+      exercises: 7,
+    },
+    {
+      name: "State of a component",
+      exercises: 14,
+    },
+  ];
 
   return (
     <>
       <Header course={course} />
-      <Content part1={part1} part2={part2} part3={part3} exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
-      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </>
-  )
-}
+  );
+};
 
-export default App
+
+
+export default App;
